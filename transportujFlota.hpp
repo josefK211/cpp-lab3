213c66df05782c041a1ad5e2d7252f9c9b9b13e9
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2022-10-24 11:08:26 +0200

    replaced instruction text with link

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,24 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0)
+        return 0; // Jeœli towar = 0, nic nie transportujemy
+
+    Stocznia     stocznia;
+    unsigned int przetransportowanyTowar = 0;
+    unsigned int liczbaZaglowcow         = 0;
+
+    while (przetransportowanyTowar < towar) {
+        Statek* statek = stocznia(); // Tworzymy statek przy u¿yciu fabryki
+        przetransportowanyTowar += statek->transportuj();
+
+        // Sprawdzamy, czy statek jest ¿aglowcem
+        if (dynamic_cast< Zaglowiec* >(statek) != nullptr) {
+            ++liczbaZaglowcow;
+        }
+
+        delete statek; // Usuwamy stworzony statek
+    }
+
+    return liczbaZaglowcow;
 }
